bf417da274dc7592953ac509e405f7907dc27b31
WEBSITESHOPPING <tranthaomeii@gmail.com>
2021-07-26 10:21:27 +0700

    init

---
+++ src/test/java/project/minhduc/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package project.minhduc.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
